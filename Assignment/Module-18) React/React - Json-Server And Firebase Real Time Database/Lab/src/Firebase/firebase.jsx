7cd7dc3995fce006eea206b435c2975bb4f09178
Uttam-51 <uttambavarva1@gmail.com>
2025-07-15 13:15:14 +0530

    update

--- Assignment/Module-18) React/React - Json-Server And Firebase Real Time Database/Lab/src/Firebase/firebase.jsx
+++ Assignment/Module-18) React/React - Json-Server And Firebase Real Time Database/Lab/src/Firebase/firebase.jsx
@@ -9,13 +9,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "firedemo-239bc.firebaseapp.com",
-  projectId: "firedemo-239bc",
-  storageBucket: "firedemo-239bc.firebasestorage.app",
-  messagingSenderId: "16536794158",
-  appId: "1:16536794158:web:6fa400245f10a7008a9bbf",
-  measurementId: "G-ZK9CBZKDPL"
+
 };
 
 
